3eb461e9d301e6bb77e3b7d5718133d05f39ff55
Mihail Dragnev <bymykel5@gmail.com>
2021-12-01 23:07:30 +0100

    refactor: return 40 movies in every request.

--- app/Services/TMDB/TmdbMoviesInformationApi.php
+++ app/Services/TMDB/TmdbMoviesInformationApi.php
@@ -14,11 +14,21 @@ class TmdbMoviesInformationApi
         $response = Http::get('https://api.themoviedb.org/3/discover/movie', [
             'api_key' => Config::get('services.tmdb.key'),
             'include_adult' => false,
-            'page' => $page
+            'page' => $page * 2 - 1
         ]);
 
         if ($response->ok()) {
             $movies = $response->json();
+        }
+
+        $response = Http::get('https://api.themoviedb.org/3/discover/movie', [
+            'api_key' => Config::get('services.tmdb.key'),
+            'include_adult' => false,
+            'page' => $page * 2
+        ]);
+
+        if ($response->ok()) {
+            $movies['results'] = array_merge($movies['results'], $response->json()['results']);
         }
 
         return $movies;
