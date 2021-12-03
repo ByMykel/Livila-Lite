710312c92571a00db45e58e5e547c2efaaed6669
Mihail Dragnev <bymykel5@gmail.com>
2021-12-03 21:03:32 +0100

    feat: add empty pages.

---
+++ resources/js/Pages/Liked.vue
@@ -0,0 +1,14 @@
+<template>
+    <app-layout title="Liked"> Liked </app-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+export default defineComponent({
+    components: {
+        AppLayout,
+    },
+});
+</script>
---
+++ resources/js/Pages/Watched.vue
@@ -0,0 +1,14 @@
+<template>
+    <app-layout title="Liked"> Watched </app-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+export default defineComponent({
+    components: {
+        AppLayout,
+    },
+});
+</script>
