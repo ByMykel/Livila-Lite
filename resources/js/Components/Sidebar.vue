c754ce0b8031b297afcc8b7ac5af5f45badae8f8
Mihail Dragnev <bymykel5@gmail.com>
2021-12-03 21:04:11 +0100

    refator: change background color.

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="h-screen space-y-0.5 bg-black-400 w-28">
+    <div class="h-screen space-y-0.5 bg-black-500 w-28">
         <div class="flex justify-center my-6">
             <img class="w-10" src="/images/logo.svg" />
         </div>
         <div class="w-full px-2" v-for="(link, index) in links" :key="index">
             <a
                 :href="route(link.route)"
-                class="block py-2 text-gray-300 rounded-md hover:text-white hover:bg-black-500"
+                class="block py-2 text-gray-300 rounded-md hover:text-white hover:bg-black-400"
             >
                 <div class="flex justify-center">
                     <HeroIconsOutline class="w-6 h-6" :name="link.icon" />
