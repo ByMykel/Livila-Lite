9b95ed530639cdb171060e44604c351019107bb0
Mihail Dragnev <bymykel5@gmail.com>
2021-12-13 22:15:00 +0100

    feat: changes in click and dbclick events.
    Don't trigger click when the user dbclick and add new animation when you like a movie.

--- resources/js/Components/Movie.vue
+++ resources/js/Components/Movie.vue
@@ -4,8 +4,8 @@
             tabindex="0"
             class="
                 block
+                relative
                 rounded-md
-                group
                 p-0.5
                 border-transparent border-2
                 focus:border-purple-300
@@ -17,12 +17,45 @@
             :class="{
                 'border-purple-500 focus:border-purple-500': isSelected(movie),
             }"
-            @click="selectMovie(movie)"
-            @keydown.enter="selectMovie(movie)"
-            @keydown.esc="selectMovie(null)"
+            @click="handleClick()"
+            @keydown.esc="hideMovie()"
         >
+            <transition name="heart-icon">
+                <div
+                    v-show="movie.liked"
+                    class="
+                        absolute
+                        flex
+                        justify-center
+                        items-end
+                        rounded-b-md
+                        top-0.5
+                        right-2.5
+                        sm:w-5
+                        w-4
+                        sm:h-6
+                        h-5
+                        bg-red-500
+                    "
+                >
+                    <transition
+                        enter-active-class="transition duration-200 ease-in"
+                        enter-from-class="opacity-0"
+                        enter-to-class="opacity-100"
+                        leave-active-class="transition duration-75 ease-in"
+                        leave-from-class="opacity-100"
+                        leave-to-class="opacity-0"
+                    >
+                        <HeroIconsOutline
+                            v-show="movie.liked"
+                            name="heart"
+                            class="w-3 h-3 sm:w-4 sm:h-4 text-white mb-0.5"
+                        />
+                    </transition>
+                </div>
+            </transition>
             <img
-                class="block object-cover h-full rounded-md cursor-pointer pointer-events-none bg-black-300"
+                class="block object-cover h-full rounded-md cursor-pointer bg-black-300"
                 :class="{ 'animate-pulse': skeleton }"
                 :src="image(movie)"
                 @load="skeleton = false"
@@ -42,15 +75,22 @@
 <script>
 import { defineComponent } from "vue";
 import { mapActions, mapGetters } from "vuex";
+import HeroIconsOutline from "@/Components/HeroIconsOutline.vue";
 
 export default defineComponent({
     props: {
         movie: Object,
     },
 
+    components: {
+        HeroIconsOutline,
+    },
+
     data() {
         return {
             skeleton: true,
+            clicks: 0,
+            timer: null,
         };
     },
 
@@ -59,7 +99,7 @@ export default defineComponent({
     },
 
     methods: {
-        ...mapActions("movie", ["selectMovie"]),
+        ...mapActions("movie", ["selectMovie", "showMovie", "hideMovie"]),
         image(movie) {
             if (movie.backdrop_path == null) {
                 return "images/placeholder.jpeg";
@@ -68,8 +108,60 @@ export default defineComponent({
             return "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
         },
         isSelected(movie) {
+            if (this.getSelectedMovie?.id === undefined) {
+                return false;
+            }
+
             return movie.id === this.getSelectedMovie?.id;
+        },
+        handleLike() {
+            this.$inertia.post(
+                route("movies.like", this.movie.id),
+                {},
+                {
+                    preserveState: true,
+                    preserveScroll: true,
+                    resetOnSuccess: false,
+                }
+            );
+
+            this.movie.liked = !this.movie.liked;
+        },
+        handleClick() {
+            this.clicks++;
+            if (this.clicks === 1) {
+                this.timer = setTimeout(() => {
+                    this.selectMovie({ ...this.movie });
+                    this.showMovie();
+                    this.clicks = 0;
+                }, 300);
+            } else {
+                clearTimeout(this.timer);
+                this.clicks = 0;
+                this.handleLike();
+
+                if (this.movie.id === this.getSelectedMovie?.id) {
+                    this.selectMovie({ ...this.movie });
+                }
+            }
         },
     },
 });
 </script>
+
+<style scoped>
+.heart-icon-enter-active,
+.heart-icon-leave-active {
+    transition: height 0.2s;
+}
+.heart-icon-enter-active {
+    height: 1.5rem;
+}
+.heart-icon-enter-from,
+.heart-icon-leave-active {
+    height: 0;
+}
+.heart-icon-leave-from {
+    height: 1.5rem;
+}
+</style>
