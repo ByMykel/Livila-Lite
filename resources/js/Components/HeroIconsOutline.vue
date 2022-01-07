63e6a5d5feed2b1a668d2796eeebf4cc1d355dab
Mihail Dragnev <bymykel5@gmail.com>
2022-01-07 21:44:41 +0100

    refactor: change icon.

--- resources/js/Components/HeroIconsOutline.vue
+++ resources/js/Components/HeroIconsOutline.vue
@@ -66,7 +66,7 @@
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7"
+            d="M4 6h16M4 12h16M4 18h16"
         ></path>
         <!-- Icon: cog -->
         <path
