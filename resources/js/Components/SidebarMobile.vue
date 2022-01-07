df713a2f11f2e7b11c9edb970d8b161127fda24b
Mihail Dragnev <bymykel5@gmail.com>
2022-01-07 22:00:53 +0100

    refactor: add link to log in/sing up and user information.

--- resources/js/Components/SidebarMobile.vue
+++ resources/js/Components/SidebarMobile.vue
@@ -1,13 +1,40 @@
 <template>
     <div
-        class="z-50 flex items-center w-full h-12 shadow bg-black-500 md:hidden"
+        class="z-50 flex items-center justify-between w-full h-12 shadow bg-black-500 md:hidden"
     >
-        <button
-            class="h-full px-2.5 border-r border-black-400"
-            @click="showSidebar = !showSidebar"
-        >
-            <HeroIconsOutline name="menu-alt2" class="w-6 h-6 text-gray-300" />
-        </button>
+        <div class="flex justify-center ml-5">
+            <img class="w-9" src="/images/logo.svg" />
+        </div>
+        <div class="flex items-center h-full">
+            <div class="mr-2" v-if="$page.props.user === null">
+                <a
+                    :href="route('login')"
+                    class="flex items-center w-full px-2 py-2 text-gray-300 rounded-md hover:text-white focus:text-white hover:bg-black-400 focus:bg-black-400"
+                >
+                    <div class="flex justify-center text-sm">Sign In</div>
+                </a>
+            </div>
+            <div class="mr-2" v-if="$page.props.user === null">
+                <a
+                    :href="route('register')"
+                    class="flex items-center w-full px-2 py-2 text-gray-300 rounded-md hover:text-white focus:text-white hover:bg-black-400 focus:bg-black-400"
+                >
+                    <div class="flex justify-center text-sm">Sign Up</div>
+                </a>
+            </div>
+            <div v-else class="relative mr-2.5">
+                <Dropdown />
+            </div>
+            <button
+                class="h-full px-2.5 border-l border-black-400"
+                @click="showSidebar = !showSidebar"
+            >
+                <HeroIconsOutline
+                    name="menu-alt2"
+                    class="w-6 h-6 text-gray-300"
+                />
+            </button>
+        </div>
         <div v-if="showSidebar" class="relative">
             <div
                 class="fixed inset-0 w-full h-screen bg-opacity-80 bg-black-400"
@@ -44,28 +71,6 @@
                             </a>
                         </div>
                     </div>
-                    <div v-if="$page.props.user === null" class="w-full p-2">
-                        <a
-                            :href="route('login')"
-                            class="block py-2 text-gray-300 rounded-md hover:text-green-600 focus:text-green-600 hover:bg-black-400 focus:bg-black-400"
-                        >
-                            <div class="flex justify-center">
-                                <HeroIconsOutline class="w-6 h-6" name="lock-open" />
-                            </div>
-                            <div class="flex justify-center">Log In</div>
-                        </a>
-                    </div>
-                    <div v-else class="w-full p-2">
-                        <button
-                            class="block w-full py-2 text-gray-300 rounded-md hover:text-red-600 focus:text-red-600 hover:bg-black-400 focus:bg-black-400"
-                            @click="logout()"
-                        >
-                            <div class="flex justify-center">
-                                <HeroIconsOutline class="w-6 h-6" name="lock-closed" />
-                            </div>
-                            <div class="flex justify-center">Log Out</div>
-                        </button>
-                    </div>
                 </div>
             </div>
         </div>
@@ -76,11 +81,13 @@
 import { defineComponent } from "vue";
 import HeroIconsOutline from "@/Components/HeroIconsOutline.vue";
 import Sidebar from "@/Components/Sidebar.vue";
+import Dropdown from "@/Components/Dropdown.vue";
 
 export default defineComponent({
     components: {
         HeroIconsOutline,
         Sidebar,
+        Dropdown,
     },
     data() {
         return {
